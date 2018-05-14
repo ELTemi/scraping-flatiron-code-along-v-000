8e3127fdae017f6a918d0206587b672ff272431c
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-05-14 17:01:16 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,19 @@
+require "pry"
 require 'nokogiri'
 require 'open-uri'
 
 require_relative './course.rb'
 
 class Scraper
-  
+
+    def get_page
+      
+    end
+
+    def get_courses
+      #courses = doc.css(".posts-holder .h2")
+    end
+
     def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -15,8 +24,7 @@ class Scraper
       end
     end
   end
-  
+
 end
 
-
-
+Scraper.new.get_page
